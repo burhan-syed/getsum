4c9c431b0c76c6891d656ebac0ab051fc877b1c3
Burhan Syed <busyed98@gmail.com>
2023-12-21 14:13:06 -0500

    assume db env var

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -4,11 +4,12 @@ import postgres from 'postgres';
 const DB_URI = process.env.DB_URI;
 
 if (!DB_URI) {
-  throw new Error("Missing DB URI");
+  // throw new Error("Missing DB URI");
+  console.error("Missing DB URI");
 }
 
 // for query purposes
-const queryClient = postgres(DB_URI);
+const queryClient = postgres(DB_URI!);
 const db = drizzle(queryClient);
 
 export default db;
