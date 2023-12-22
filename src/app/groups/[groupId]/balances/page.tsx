2f11a76c48dc05d26e56229c4c29bc4bb087fc86
Burhan Syed <busyed98@gmail.com>
2023-12-21 21:13:54 -0500

    update balances page

--- src/app/groups/[groupId]/balances/page.tsx
+++ src/app/groups/[groupId]/balances/page.tsx
@@ -1,5 +1,6 @@
 import SettleButton from "@/components/forms/SettleButton";
 import { getExpenses, settleExpenses } from "@/lib/db/queries";
+import { cn } from "@/lib/utils";
 import { revalidatePath } from "next/cache";
 
 type BalancesPageProps = {
@@ -26,22 +27,49 @@ async function handleSettleExpenses({
 
 export default async function BalancesPage({ params }: BalancesPageProps) {
   const { balances } = await getExpenses({ groupId: params.groupId });
+  type User = { id: number; fullName: string };
+  const sums: Record<
+    number,
+    {
+      user: User;
+      others: Record<number, { user: User; amount: 0 }>;
+    }
+  > = {};
+  Object.values(balances).map((b) => {
+    sums[b.user.id] = {
+      user: b.user,
+      others: {},
+    };
+    Object.values(b.debts).forEach((d) => {
+      if (!sums[b.user.id].others[d.user.id]) {
+        sums[b.user.id].others[d.user.id] = { user: d.user, amount: 0 };
+      }
+      sums[b.user.id].others[d.user.id].amount -= Math.abs(d.amount);
+    });
+    Object.values(b.credits).forEach((c) => {
+      if (!sums[b.user.id].others[c.user.id]) {
+        sums[b.user.id].others[c.user.id] = { user: c.user, amount: 0 };
+      }
+      sums[b.user.id].others[c.user.id].amount += Math.abs(c.amount);
+    });
+  });
+
   return (
     <div className="">
       <h3 className="h-8 border-b sticky top-16 px-4 flex items-center font-semibold">
         Group Balances
       </h3>
       <ul className="divide-y">
-        {Object.values(balances).map((b) => (
+        {Object.values(sums).map((s) => (
           <li
-            key={`${b.user.id}_balances`}
+            key={`${s.user.id}_balances`}
             className="flex justify-between text-sm p-1 px-4"
           >
             <div className="flex flex-col justify-between">
-              <h3>{b.user.fullName}</h3>
-              {Object.values(b.debts).length > 0 && (
+              <h3>{s.user.fullName}</h3>
+              {Object.values(s.others).some(o => o.amount < 0) && (
                 <SettleButton
-                  userId={b.user.id}
+                  userId={s.user.id}
                   groupId={params.groupId}
                   handleSubmit={handleSettleExpenses}
                 />
@@ -50,24 +78,22 @@ export default async function BalancesPage({ params }: BalancesPageProps) {
 
             <div className="text-right">
               <ul>
-                {Object.values(b.debts).map((d) => (
-                  <li key={`${d.user.id}_debt_${d.amount}`}>
-                    <span className="text-destructive">
-                      {d.amount.toFixed(2)}
-                    </span>{" "}
-                    owed to {d.user.fullName}
-                  </li>
-                ))}
-              </ul>
-              <ul>
-                {Object.values(b.credits).map((c) => (
-                  <li key={`${c.user.id}_credit_${c.amount}`}>
-                    <span className="text-green-700">
-                      {c.amount.toFixed(2)}
-                    </span>{" "}
-                    owed by {c.user.fullName}
-                  </li>
-                ))}
+                {Object.values(s.others)
+                  .sort((a, b) => a.user.fullName.localeCompare(b.user.fullName))
+                  .map((o) => (
+                    <li key={`${o.user.id}_balance_to_${s.user.id}}`}>
+                      <span
+                        className={cn(
+                          o.amount >= 0 ? "text-green-700" : "text-destructive"
+                        )}
+                      >
+                        {Math.abs(o.amount).toFixed(2)}
+                      </span>
+                      {` owed ${o.amount >= 0 ? "from" : "to"} ${
+                        o.user.fullName
+                      }`}
+                    </li>
+                  ))}
               </ul>
             </div>
           </li>
